a045b12db13603799c689aae5cae33a44a871876
DeveshBahuguna <bahuguna.devesh95@gmail.com>
2020-09-26 19:51:20 +0530

    FTP Clinet for .NET 5/.NET Core

---
+++ FTPFileTransfer/FTPHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FTPFileTransfer
+{
+    class FTPHelper
+    {
+        public static string FtpServerIp = string.Empty;
+        public static string Username = string.Empty;
+        public static string Password = string.Empty;
+
+
+        public static void CreateFTPDirectory(string directoryName)
+        {
+            WebRequest ftpRequest = WebRequest.Create("ftp://" + FtpServerIp+"/"+directoryName);
+            ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+            ftpRequest.Credentials = new NetworkCredential(Username, Password);
+            ftpRequest.GetResponse();
+        }
+        
+
+        public static void DownloadFile(string fileNameWithPath)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + FtpServerIp+"//"+fileNameWithPath);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential(Username, Password);
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            string fileName = fileNameWithPath;
+            var file = File.Create(fileName);
+            responseStream.CopyTo(file);
+            file.Close();
+            Console.WriteLine($"Download Complete, status {response.StatusDescription}");
+            response.Close();
+        }
+
+        public static void DownloadDirectory(string directoryNameWithPath)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + FtpServerIp + "/" + directoryNameWithPath);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            request.Credentials = new NetworkCredential(Username, Password);
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string directoryName = directoryNameWithPath;
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+            string file = reader.ReadLine();
+            while (file != null)
+            {
+                //drwxr-xr-x 1 ftp ftp              0 Mar 31 22:34 New folder
+                string[] fileDetails = file.Split(' ');
+
+                //Check if this is directory or file
+                if (fileDetails[0].Contains('d'))
+                {
+                    DownloadDirectory(directoryNameWithPath + "/" + fileDetails[fileDetails.Length - 1]);
+                }
+                else
+                {
+                    DownloadFile(directoryNameWithPath + "/" + fileDetails[fileDetails.Length - 1]);
+                }
+                file = reader.ReadLine();
+            }
+        }
+
+        public static void UploadFile(string fileName)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + FtpServerIp+"/"+fileName);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential(Username, Password);
+            byte[] uploadContents;
+            uploadContents = File.ReadAllBytes(fileName);
+            request.ContentLength = uploadContents.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(uploadContents, 0, uploadContents.Length);
+            }
+
+            using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+            {
+                Console.WriteLine($"Upload File Complete, status {resp.StatusDescription}");
+            }
+        }
+
+        public static void UploadDirectory(string directoryName)
+        {
+            CreateFTPDirectory(directoryName);
+            DirectoryInfo directory = new DirectoryInfo(directoryName);
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                UploadFile(directoryName+ "/" + file.Name);
+            }
+            foreach (var sudDirectory in directory.GetDirectories())
+            {
+                UploadDirectory(directoryName + "/" + sudDirectory.Name);
+            }
+        }
+    }
+}
---
+++ FTPFileTransfer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FTPFileTransfer
+{
+    class Program
+    {
+             
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter FTPServerIp");
+            FTPHelper.FtpServerIp = Console.ReadLine();
+            Console.WriteLine("Enter UserName_Password");
+            string[] credentials = Console.ReadLine().Split('_');
+            FTPHelper.Username = credentials[0];
+            FTPHelper.Password = credentials[1];
+            while (true)
+            {
+                Console.WriteLine("Press U for upload or D for download or E for exit");
+                string operation = Console.ReadLine();
+                Console.WriteLine("Enter 'f(file) fileName' or 'd(directory) directoryName'");
+                string[] fileDetails = Console.ReadLine().Split(' ');
+
+                switch (operation.ToUpper())
+                {
+                    case "U":
+                        {
+                            if (fileDetails[0].ToUpper() == "F")
+                            {
+                                FTPHelper.UploadFile(fileDetails[1]);
+                            }
+                            else
+                            {
+                                FTPHelper.UploadDirectory(fileDetails[1]);
+                            }
+                            break;
+                        }
+                    case "D":
+                        {
+                            if (fileDetails[0].ToUpper() == "F")
+                            {
+                                FTPHelper.DownloadFile(fileDetails[1]);
+                            }
+                            else
+                            {
+                                FTPHelper.DownloadDirectory(fileDetails[1]);
+                            }
+                            break;
+                        }
+                    default: return;
+                }
+                
+            }
+        }
+    }
+}
